712f8d2e9ee872441ebdd81259183d8b45c79979
JonidaTahiraj <tahirajjonida868@gmail.com>
2024-07-17 12:40:38 +0200

    part3

---
+++ JAVA3/my-first-react-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>Hello world, this is my first react app.</p>
+    </div>
+  );
+}
+
+export default App;
